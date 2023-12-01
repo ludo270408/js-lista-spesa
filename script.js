f76985e7303760ec90b53dcc0c1a676f577981ce
ludo270408 <ludovica.sette7@gmaio.com>
2023-12-01 23:35:02 +0100

    [UPTADE] List done and counter added

---
+++ script.js
@@ -0,0 +1,7 @@
+//Creiamo la lista della spesa
+const shopping_list =['latte', 'uova', 'pane', 'pasta', 'passata di pomodoro', 'carta igenica'];
+ const ul = document.getElementById('shopping-list');
+
+ //Contatore
+ let i=0;
+ 
